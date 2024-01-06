9944c68ba67c139614b4179b4556c2b85b972c55
bkkhyunn <toiquen419@gmail.com>
2024-01-06 16:24:03 +0900

    [feat] message queue 기반 gpu 자동학습 툴 제작
    - message queue 를 활용한 gpu 자동학습 툴입니다.
    - 조창희 캠퍼님께서 작성해주신 것을 저희 팀에 맞게 일부분 수정했습니다.
    
    #7

---
+++ gpu_pool/env.py
@@ -0,0 +1,24 @@
+import os
+
+CWD = os.getcwd()
+
+# MySQL 서버 관련
+MYSQL_SERVER_IP = "10.28.224.90"
+MYSQL_SERVER_PORT = 30150
+MYSQL_SERVER_USER = "CV-06"
+MYSQL_SERVER_PASSWORD = "cv06"
+
+# 이름
+CAMPER_NAME = "bkh"
+
+# 큐에 푸시하려는 메시지를 작성한 파일의 주소
+MESSAGE_FILE_PATH = "./message.json"
+
+# id, branch, train_path, receiver, pushed의 위치를 수정하지 마세요.
+MESSAGE_COLUMNS = ["id", "branch", "train_path", "receiver", "name", "seed", "pushed"]
+
+# 이하 수정 x
+ARGS_EXCEPTION = ["id", "branch", "train_path", "receiver", "pushed"]
+BRANCH_INDEX = 1
+TRAIN_INDEX = 2
+RECEIVER_INDEX = 3
